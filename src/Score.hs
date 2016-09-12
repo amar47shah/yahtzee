a0f93b33493a9caa2cf4e3f12b926c0f806f908f
Amar Shah <amar47shah@gmail.com>
2016-09-12 00:11:28 -0400

    Add scores for all types of rolls

--- src/Score.hs
+++ src/Score.hs
@@ -1,1 +1,97 @@
 module Score where
+
+import Data.List (sort, tails)
+
+data Die = One
+         | Two
+         | Three
+         | Four
+         | Five
+         | Six
+         deriving (Enum, Eq, Ord, Show)
+
+dice :: [Die]
+dice = enumFrom One
+
+type Value = Int
+
+value :: Die -> Value
+value = succ . fromEnum
+
+fromValue :: Value -> Die
+fromValue = toEnum . pred
+
+zeroValue :: Value
+zeroValue = 0
+
+type Roll = [Die]
+
+fromValues :: [Value] -> Roll
+fromValues = fmap fromValue
+
+values :: Roll -> [Value]
+values = fmap value
+
+type Scoring = Roll -> Value
+
+countAndAddOnly :: Die -> Scoring
+countAndAddOnly d = (* value d) . length . filter (== d)
+
+type Check   = Roll -> Bool
+data Special = Special { check   :: Check
+                       , scoring :: Scoring
+                       }
+
+scoreIf :: Special -> Scoring
+scoreIf s = \r ->
+  case check s r of
+    True -> scoring s r
+    _    -> zeroValue
+
+threeOfAKind :: Special
+threeOfAKind  = Special { check = hasOfAKind 3, scoring = sumOfValues }
+
+fourOfAKind :: Special
+fourOfAKind  = Special { check = hasOfAKind 4, scoring = sumOfValues }
+
+chance :: Special
+chance  = Special { check = const True, scoring = sumOfValues }
+
+sumOfValues :: Scoring
+sumOfValues = sum . values
+
+yahtzee :: Special
+yahtzee  = Special { check = hasOfAKind 5, scoring = const 50 }
+
+hasOfAKind :: Int -> Check
+hasOfAKind n = any (>= n) . kindCounts
+
+fullHouse :: Special
+fullHouse  = Special { check = isFullHouse, scoring = const 25 }
+
+isFullHouse :: Check
+isFullHouse = (`elem` [[2,3], [3,2]]) . filter (/= 0) . kindCounts
+
+kindCounts :: Roll -> [Int]
+kindCounts r = (`kindCount` r) <$> dice
+
+kindCount :: Die -> Roll -> Int
+kindCount d = length . filter (== d)
+
+smallStraight :: Special
+smallStraight  = Special { check = hasStraight 4, scoring = const 30 }
+
+largeStraight :: Special
+largeStraight  = Special { check = hasStraight 5, scoring = const 40 }
+
+hasStraight :: Int -> Check
+hasStraight n = any isStraight . windowsOfSorted n
+
+isStraight :: Check
+isStraight [] = True
+isStraight r  = let vs = values r
+                 in all (== 1) $ zipWith (-) (tail vs) vs
+
+windowsOfSorted :: Int -> Roll -> [Roll]
+windowsOfSorted n r = let m = 1 - n + length r
+                       in map (take n) . take m . tails $ sort r
