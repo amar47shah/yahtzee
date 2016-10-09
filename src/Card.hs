52f51fb60fee4fdfa7fe2be811d1df843614657e
Amar Shah <amar47shah@gmail.com>
2016-10-09 13:47:43 -0400

    Add totalling functions for Card

--- src/Card.hs
+++ src/Card.hs
@@ -4,6 +4,7 @@ import Roll (Face, Value, faces)
 import Score (Score, Special, specials)
 import Utilities ((...))
 
+import Control.Applicative (liftA2)
 import qualified Data.Map.Strict as M
 
 data Game = Game { upper :: UpperSection
@@ -24,3 +25,18 @@ initialLower = fromKeysWith Nothing specials
 
 fromKeysWith :: Ord a => b -> [a] -> M.Map a b
 fromKeysWith = M.fromList ... fmap . flip (,)
+
+total :: Game -> Score
+total g = (+) <$> upperTotal g <*> lowerTotal g
+
+upperTotal :: Game -> Score
+upperTotal = fmap addBonus . sumA . upper
+  where addBonus n
+         | n >= 63   = n + 35
+         | otherwise = n
+
+lowerTotal :: Game -> Score
+lowerTotal = sumA . lower
+
+sumA :: (Num a, Foldable t, Applicative f) => t (f a) -> f a
+sumA = foldr (liftA2 (+)) $ pure 0
