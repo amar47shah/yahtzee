6898970ac08c755e5ef1a15dd02b93ba86785e17
Amar Shah <amar47shah@gmail.com>
2016-10-09 14:44:38 -0400

    Rename Game to Card

--- src/Card.hs
+++ src/Card.hs
@@ -6,14 +6,14 @@ import Utilities ((...))
 import Control.Applicative (liftA2)
 import qualified Data.Map.Strict as M
 
-data Game = Game { upper :: Section
+data Card = Card { upper :: Section
                  , lower :: Section
                  } deriving Show
 
 type Section = M.Map Combo Score
 
-initial :: Game
-initial = Game initialUpper initialLower
+initial :: Card
+initial = Card initialUpper initialLower
 
 initialUpper :: Section
 initialUpper = fromKeysWith Nothing counters
@@ -24,16 +24,16 @@ initialLower = fromKeysWith Nothing specials
 fromKeysWith :: Ord a => b -> [a] -> M.Map a b
 fromKeysWith = M.fromList ... fmap . flip (,)
 
-total :: Game -> Score
+total :: Card -> Score
 total g = (+) <$> upperTotal g <*> lowerTotal g
 
-upperTotal :: Game -> Score
+upperTotal :: Card -> Score
 upperTotal = fmap addBonus . sumA . upper
   where addBonus n
          | n >= 63   = n + 35
          | otherwise = n
 
-lowerTotal :: Game -> Score
+lowerTotal :: Card -> Score
 lowerTotal = sumA . lower
 
 sumA :: (Num a, Foldable t, Applicative f) => t (f a) -> f a
