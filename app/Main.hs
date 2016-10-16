81d321d0b750f1f6e464cc92578915fcff12b02c
Amar Shah <amar47shah@gmail.com>
2016-10-15 21:56:11 -0400

    Use random rerolls and print card between turns

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,28 @@
 module Main where
 
 import Card (Card, initial, bank, total)
-import Roll (reroll, roll)
+import Roll (Roll, reroll, roll)
 import Data.Maybe (isJust)
+import System.Random (randomRIO)
 
 main :: IO Card
 main = untilM (isJust . total) turn initial
 
 turn :: Card -> IO Card
-turn c =
-  roll
-    >>= (`reroll` [])
-      >>= (`reroll` [])
-        >>= \r -> print r >> pure r
-          >>= \r -> bank c r <$> getLine
+turn c = print c
+      *> roll
+     >>= printAndReturn
+     >>= randomReroll
+     >>= printAndReturn
+     >>= randomReroll
+     >>= printAndReturn
+     >>= (<$> getLine) . bank c
+
+randomReroll :: Roll -> IO Roll
+randomReroll r = (`splitAt` r) <$> randomRIO (0, length r) >>= uncurry reroll
+
+printAndReturn :: Show a => a -> IO a
+printAndReturn x = print x *> pure x
 
 untilM :: Monad m => (a -> Bool) -> (a -> m a) -> a -> m a
 untilM p k x
